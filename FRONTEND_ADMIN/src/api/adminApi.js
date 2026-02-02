955c423fad2c107eca0b3d60d96a1834a391bbdd
Yash Mahajan <124796269+Ajook1@users.noreply.github.com>
2026-02-02 12:04:35 +0530

    added the different url to access backend

--- FRONTEND_ADMIN/src/api/adminApi.js
+++ FRONTEND_ADMIN/src/api/adminApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const adminApi = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/admin`
+  baseURL: `${import.meta.env.VITE_API_URL}/admin`
 })
 
 export default adminApi
